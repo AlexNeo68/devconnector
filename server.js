162eea65f4ad1928d440e1fabff4bb5f4ae87261
AlexNeo68 <tunning_brain@mail.ru>
2019-01-20 09:30:10 +0300

    prepared deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const app = express();
 
@@ -34,6 +35,13 @@ const posts = require("./routes/api/posts");
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5005;
 
